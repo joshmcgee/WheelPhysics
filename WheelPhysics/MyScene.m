3227ef1994fbef79b74c3ecb37809d39ceec3b39
Joshua McGee <talidos.josh@gmail.com>
2013-08-01 18:10:56 -0400

    Collisions are working.

--- WheelPhysics/MyScene.m
+++ WheelPhysics/MyScene.m
@@ -9,9 +9,17 @@
 #import "MyScene.h"
 #import "WheelSpriteNode.h"
 
+@interface MyScene ()
+
+
+
+@end
+
 @implementation MyScene
 
--(id)initWithSize:(CGSize)size {    
+static const uint32_t wheelCategory = 0x1 << 0;
+
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
@@ -21,20 +29,45 @@
         self.physicsWorld.contactDelegate = self;
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         
-        WheelSpriteNode *wheel = (WheelSpriteNode *)[[SKSpriteNode alloc] initWithTexture:[SKTexture textureWithImageNamed:@"dummy-circle"] color:nil size:CGSizeMake(32.0, 32.0)];
-        wheel.position = CGPointMake(160.0, 720.0);
-        wheel.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:wheel.size.width/2];
+//        WheelSpriteNode *wheel = [[WheelSpriteNode alloc] initWithTexture:[SKTexture textureWithImageNamed:@"dummy-circle"] color:nil size:CGSizeMake(32.0, 32.0)];
+//        wheel.position = CGPointMake(160.0, 720.0);
+        //wheel.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:wheel.size.width/2];
+        SKSpriteNode *sprite = [self newSpriteWithSpriteName:@"dummy-circle" withPosition:CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)) name:@"Wheel"];
         
-        SKAction *rotate = [SKAction rotateByAngle:M_PI duration:1.0];
-        [wheel runAction:[SKAction repeatActionForever:rotate]];
+        //SKAction *rotate = [SKAction rotateByAngle:M_PI duration:1.0];
+        //[sprite runAction:[SKAction repeatActionForever:rotate]];
         
-        [self addChild:wheel];
+        [self addChild:sprite];
+        
+//        SKSpriteNode *sprite2 = [self newSpriteWithSpriteName:@"dummy-circle" withPosition:CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame)) name:@"Wheel02"];
+//        
+//        SKAction *rotate2 = [SKAction rotateByAngle:M_PI duration:1.0];
+//        [sprite2 runAction:[SKAction repeatActionForever:rotate2]];
+//        
+//        [self addChild:sprite2];
+        
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+- (SKSpriteNode *)newSpriteWithSpriteName:(NSString *)spriteName withPosition:(CGPoint)position name:(NSString *)name
+{
+    SKSpriteNode *sprite = [[SKSpriteNode alloc] initWithImageNamed:spriteName];
+    sprite.position = position;//CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    sprite.name = name;
+    sprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:sprite.size.width/2];
+    [sprite.physicsBody setDynamic:YES];
+//    sprite.physicsBody.usesPreciseCollisionDetection = YES;
+    sprite.physicsBody.categoryBitMask = wheelCategory;
+    sprite.physicsBody.collisionBitMask = wheelCategory;
+    sprite.physicsBody.contactTestBitMask = wheelCategory;
+    return sprite;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
+    
+    //NSLog(@"Touched!");
     
     for (UITouch *touch in touches) {
         //CGPoint location = [touch locationInNode:self];
@@ -54,12 +87,40 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+   // NSLog(@"update");
 }
 
 - (void)didBeginContact:(SKPhysicsContact *)contact {
     
     
     NSLog(@"Scene did begin contact.");
+    
+    SKPhysicsBody *wheel;
+    if (contact.bodyA.categoryBitMask == wheelCategory) {
+        wheel = contact.bodyA;
+    }
+    else {
+        wheel = contact.bodyB;
+    }
+    
+//    SKPhysicsBody *firstBody, *secondBody;
+//    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask)
+//    {
+//        firstBody = contact.bodyA;
+//        secondBody = contact.bodyB;
+//    }
+//    else {
+//        firstBody = contact.bodyB;
+//        secondBody = contact.bodyA;
+//    }
+//    if ((firstBody.categoryBitMask & missileCategory) != 0)
+//    {
+//        [self attack: secondBody.node withMissile:firstBody.node];
+//        2013-06-10 | Copyright © 2013 Apple Inc. All Rights Reserved. Apple Confidential Information. 98
+//        ￼￼￼￼
+//        Simulating Physics
+//        Working with Collisions and Contacts
+//    }
 }
 
 @end
